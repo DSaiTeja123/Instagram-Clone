94202de218d2a6142e2e5d1a6b952ef5d6b98f08
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-03-24 13:28:58 +0530

    updated routing

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { 
   registerController,
-  // googleSignupController,
   loginController,
   logoutController,
   fetchProfile,
@@ -9,22 +8,23 @@ import {
   getSuggestedUsers,
   followUnfollow,
   deleteUserController
-}
-from "../controllers/user.controller.js";
+} from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.post('/https://instagram-clone-eptf.onrender.com/signup', registerController);
-// router.post('/signup', googleSignupController);
-router.post('/https://instagram-clone-eptf.onrender.com/signin', loginController);
-router.get('/https://instagram-clone-eptf.onrender.com/logout', isAuthenticated, logoutController);
+router.post('/signup', registerController);
+router.post('/signin', loginController);
+router.get('/logout', isAuthenticated, logoutController);
+
 router
-  .route('/https://instagram-clone-eptf.onrender.com/:id/profile')
+  .route('/:id/profile')
   .get(isAuthenticated, fetchProfile)
   .delete(isAuthenticated, deleteUserController);
-router.post('/https://instagram-clone-eptf.onrender.com/profile/update', isAuthenticated, upload.single('profilePhoto'), updateProfile);
-router.get('/https://instagram-clone-eptf.onrender.com/suggested', isAuthenticated, getSuggestedUsers);
-router.post('/https://instagram-clone-eptf.onrender.com/follow/:id', isAuthenticated, followUnfollow);
+
+router.post('/profile/update', isAuthenticated, upload.single('profilePhoto'), updateProfile);
+router.get('/suggested', isAuthenticated, getSuggestedUsers);
+router.post('/follow/:id', isAuthenticated, followUnfollow);
+
 export default router;
