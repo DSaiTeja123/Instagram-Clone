0693b9039547e6c2574dd70d95aee167e5e9ec47
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-03-24 13:42:03 +0530

    updated origin

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import router from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
+// import path from "path";
 
 dotenv.config();
 
@@ -14,26 +15,15 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({ extended: true }));
-
-// CORS Configuration
-const allowedOrigins = [
-  "https://instagram-clone-blond-two.vercel.app",
-  "http://instagram-clone-blond-two.vercel.app"
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(urlencoded({extended: true}));
+const corsOptions = {
+  origin: [
+    "https://instagram-clone-blond-two.vercel.app",
+    "http://instagram-clone-blond-two.vercel.app"
+  ],
+  credentials: true
+}
+app.use(cors(corsOptions));
 
 app.use("/api/v2/user", router);
 app.use("/api/v2/post", postRoute);
@@ -42,4 +32,4 @@ app.use("/api/v2/message", messageRoute);
 server.listen(PORT, () => {
   connectDB();
   console.log(`Server is listening to port ${PORT}`);
-});
+})
