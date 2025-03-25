f95c8c29a97e4c499bb126e4877a95f8afb82927
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-03-25 09:21:59 +0530

    updated socket

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -8,7 +8,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors:{
-    origin:'http://localhost:5173',
+    origin: [
+      "https://instagram-clone-blond-two.vercel.app",
+      "http://instagram-clone-blond-two.vercel.app",
+    ],
     methods:['GET','POST']
   }
 })
