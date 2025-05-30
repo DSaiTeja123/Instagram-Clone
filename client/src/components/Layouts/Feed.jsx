b2304e3b3707c26fee78286c13f418e02668865c
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-05-30 16:48:51 +0530

    updated routes

--- client/src/components/Layouts/Feed.jsx
+++ client/src/components/Layouts/Feed.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Posts } from '../index'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Posts } from "../index";
+import { useSelector } from "react-redux";
 
 function Feed() {
   const { colorToggled } = useSelector((store) => store.auth);
 
+  useEffect(() => {
+    if (!user) {
+      navigate("/signin");
+    }
+  }, [user]);
+
   return (
-    <div className={`flex-1 my-8 flex flex-col items-center pl-[20%] transition-colors duration-500 ${colorToggled ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+    <div
+      className={`flex-1 my-8 flex flex-col items-center pl-[20%] transition-colors duration-500 ${
+        colorToggled ? "bg-gray-900 text-white" : "bg-white text-black"
+      }`}
+    >
       <Posts />
     </div>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
--- client/src/components/Layouts/MainLayout.jsx
+++ client/src/components/Layouts/MainLayout.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { LeftSidebar } from '../index'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { LeftSidebar } from "../index";
+import { useSelector } from "react-redux";
 
 function MainLayout() {
   const { colorToggled } = useSelector((store) => store.auth);
 
+  useEffect(() => {
+    if (!user) {
+      navigate("/signin");
+    }
+  }, [user]);
+
   return (
-    <div className={`flex transition-colors duration-500 ${colorToggled ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+    <div
+      className={`flex transition-colors duration-500 ${
+        colorToggled ? "bg-gray-900 text-white" : "bg-white text-black"
+      }`}
+    >
       <LeftSidebar />
       <div className="flex-grow">
         <Outlet />
       </div>
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
